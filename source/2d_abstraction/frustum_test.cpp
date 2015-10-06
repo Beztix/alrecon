b07b8567a95a293a1ae93b6f7d8887bd36e5f8cb
Beztix <sebastian.luetzow@gmail.com>
2015-10-06 10:27:43 +0200

    reworked plane calculation of frustums

--- source/2d_abstraction/frustum_test.cpp
+++ source/2d_abstraction/frustum_test.cpp
@@ -28,194 +28,124 @@ namespace frustum{
 
 
 
-
+	//testing two frustums for intersection
 	bool doFrustumsIntesect(frust firstFrustum, frust secondFrustum) {
 
-
-		cv::Point3f vectorOne;
-		cv::Point3f vectorTwo;
-		cv::Point3f normal;		//always pointing inside the frustum!
-		cv::Point3f normalBase;
 		bool in;
-
 
 		//========== test first frustum ===========
 
 		//right plane
-		vectorOne = firstFrustum.points[frust::far_xpos_ypos] - firstFrustum.points[frust::near_xpos_ypos];
-		vectorTwo = firstFrustum.points[frust::near_xpos_yneg] - firstFrustum.points[frust::near_xpos_ypos];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = firstFrustum.points[frust::near_xpos_ypos];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = secondFrustum.points[i] -normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = secondFrustum.points[i] - firstFrustum.planeNormalBases[frust::x_pos];
+			float dot = firstFrustum.planeNormals[frust::x_pos].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
 
 		//top plane
-		vectorOne = firstFrustum.points[frust::near_xneg_yneg] - firstFrustum.points[frust::near_xpos_yneg];
-		vectorTwo = firstFrustum.points[frust::far_xpos_yneg] - firstFrustum.points[frust::near_xpos_yneg];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = firstFrustum.points[frust::near_xpos_yneg];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = secondFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = secondFrustum.points[i] - firstFrustum.planeNormalBases[frust::y_neg];
+			float dot = firstFrustum.planeNormals[frust::y_neg].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
 
 		//left plane
-		vectorOne = firstFrustum.points[frust::near_xneg_ypos] - firstFrustum.points[frust::near_xneg_yneg];
-		vectorTwo = firstFrustum.points[frust::far_xneg_yneg] - firstFrustum.points[frust::near_xneg_yneg];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = firstFrustum.points[frust::near_xneg_yneg];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = secondFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = secondFrustum.points[i] - firstFrustum.planeNormalBases[frust::x_neg];
+			float dot = firstFrustum.planeNormals[frust::x_neg].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
 
 		//bottom plane
-		vectorOne = firstFrustum.points[frust::near_xpos_ypos] - firstFrustum.points[frust::near_xneg_ypos];
-		vectorTwo = firstFrustum.points[frust::far_xneg_ypos] - firstFrustum.points[frust::near_xneg_ypos];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = firstFrustum.points[frust::near_xneg_ypos];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = secondFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = secondFrustum.points[i] - firstFrustum.planeNormalBases[frust::y_pos];
+			float dot = firstFrustum.planeNormals[frust::y_pos].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
 
 		//front plane
-		vectorOne = firstFrustum.points[frust::near_xneg_ypos] - firstFrustum.points[frust::near_xpos_ypos];
-		vectorTwo = firstFrustum.points[frust::near_xpos_yneg] - firstFrustum.points[frust::near_xpos_ypos];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = firstFrustum.points[frust::near_xpos_ypos];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = secondFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = secondFrustum.points[i] - firstFrustum.planeNormalBases[frust::z_near];
+			float dot = firstFrustum.planeNormals[frust::z_near].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
 
 		//back plane
-		vectorOne = firstFrustum.points[frust::far_xneg_yneg] - firstFrustum.points[frust::far_xpos_yneg];
-		vectorTwo = firstFrustum.points[frust::far_xpos_ypos] - firstFrustum.points[frust::far_xpos_yneg];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = firstFrustum.points[frust::far_xpos_yneg];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = secondFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = secondFrustum.points[i] - firstFrustum.planeNormalBases[frust::z_far];
+			float dot = firstFrustum.planeNormals[frust::z_far].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
-
-
-
 
 
 
 		//========== test second frustum ===========
 
 		//right plane
-		vectorOne = secondFrustum.points[frust::far_xpos_ypos] - secondFrustum.points[frust::near_xpos_ypos];
-		vectorTwo = secondFrustum.points[frust::near_xpos_yneg] - secondFrustum.points[frust::near_xpos_ypos];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = secondFrustum.points[frust::near_xpos_ypos];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = firstFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = firstFrustum.points[i] - secondFrustum.planeNormalBases[frust::x_pos];
+			float dot = secondFrustum.planeNormals[frust::x_pos].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
 
 		//top plane
-		vectorOne = secondFrustum.points[frust::near_xneg_yneg] - secondFrustum.points[frust::near_xpos_yneg];
-		vectorTwo = secondFrustum.points[frust::far_xpos_yneg] - secondFrustum.points[frust::near_xpos_yneg];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = secondFrustum.points[frust::near_xpos_yneg];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = firstFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = firstFrustum.points[i] - secondFrustum.planeNormalBases[frust::y_neg];
+			float dot = secondFrustum.planeNormals[frust::y_neg].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
 
 		//left plane
-		vectorOne = secondFrustum.points[frust::near_xneg_ypos] - secondFrustum.points[frust::near_xneg_yneg];
-		vectorTwo = secondFrustum.points[frust::far_xneg_yneg] - secondFrustum.points[frust::near_xneg_yneg];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = secondFrustum.points[frust::near_xneg_yneg];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = firstFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = firstFrustum.points[i] - secondFrustum.planeNormalBases[frust::x_neg];
+			float dot = secondFrustum.planeNormals[frust::x_neg].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
 
 		//bottom plane
-		vectorOne = secondFrustum.points[frust::near_xpos_ypos] - secondFrustum.points[frust::near_xneg_ypos];
-		vectorTwo = secondFrustum.points[frust::far_xneg_ypos] - secondFrustum.points[frust::near_xneg_ypos];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = secondFrustum.points[frust::near_xneg_ypos];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = firstFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = firstFrustum.points[i] - secondFrustum.planeNormalBases[frust::y_pos];
+			float dot = secondFrustum.planeNormals[frust::y_pos].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
 
 		//front plane
-		vectorOne = secondFrustum.points[frust::near_xneg_ypos] - secondFrustum.points[frust::near_xpos_ypos];
-		vectorTwo = secondFrustum.points[frust::near_xpos_yneg] - secondFrustum.points[frust::near_xpos_ypos];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = secondFrustum.points[frust::near_xpos_ypos];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = firstFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = firstFrustum.points[i] - secondFrustum.planeNormalBases[frust::z_near];
+			float dot = secondFrustum.planeNormals[frust::z_near].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
 
 		//back plane
-		vectorOne = secondFrustum.points[frust::far_xneg_yneg] - secondFrustum.points[frust::far_xpos_yneg];
-		vectorTwo = secondFrustum.points[frust::far_xpos_ypos] - secondFrustum.points[frust::far_xpos_yneg];
-		normal = vectorOne.cross(vectorTwo);
-		normalBase = secondFrustum.points[frust::far_xpos_yneg];
-
 		in = false;
 		for (int i = 0; i < 8; i++) {
-			cv::Point3f testVector = firstFrustum.points[i] - normalBase;
-			float dot = normal.dot(testVector);
+			cv::Point3f testVector = firstFrustum.points[i] - secondFrustum.planeNormalBases[frust::z_far];
+			float dot = secondFrustum.planeNormals[frust::z_far].dot(testVector);
 			if (dot >= 0) in = true;
 		}
 		if (!in) return false;
-
 
 
 		//none of the planes separates the two frustums: there is an intersection
--- source/2d_abstraction/frustum_util.h
+++ source/2d_abstraction/frustum_util.h
@@ -12,16 +12,12 @@ namespace frustum {
 
 
 
-
-
-
-	
-
 	// a struct representing a single frustum
-
 	struct frust {
 
-		cv::Point3f points[8];
+		cv::Point3f points[8];				//the 8 corners of the frustum
+		cv::Point3f planeNormals[6];		//the normals of the 6 planes, always pointing inside the frustum
+		cv::Point3f planeNormalBases[6];	//the base corners of the 6 normals
 
 		enum vertex_type
 		{
@@ -29,7 +25,13 @@ namespace frustum {
 			far_xneg_ypos, far_xpos_ypos, far_xpos_yneg, far_xneg_yneg
 		};
 
+		enum plane_type
+		{
+			x_neg, x_pos, y_neg, y_pos, z_near, z_far
+		};
 
+
+		//constructor receiving the 8 corner points, automatically building the plane information
 		frust
 			(const cv::Point3f &n_xn_yp, const cv::Point3f &n_xp_yp,
 			const cv::Point3f &n_xp_yn, const cv::Point3f &n_xn_yn,
@@ -45,6 +47,54 @@ namespace frustum {
 			points[far_xpos_ypos] = f_xp_yp;
 			points[far_xpos_yneg] = f_xp_yn;
 			points[far_xneg_yneg] = f_xn_yn;	
+
+			build_planes();
+		}
+
+
+		//building the plane information
+		void build_planes() {
+			cv::Point3f vectorOne;
+			cv::Point3f vectorTwo;
+			cv::Point3f normal;
+			cv::Point3f normalBase;
+
+			//x_pos
+			vectorOne = points[frust::far_xpos_ypos] - points[frust::near_xpos_ypos];
+			vectorTwo = points[frust::near_xpos_yneg] - points[frust::near_xpos_ypos];
+			planeNormals[frust::x_pos] = vectorOne.cross(vectorTwo);
+			planeNormalBases[frust::x_pos] = points[frust::near_xpos_ypos];
+
+			//y_neg
+			vectorOne = points[frust::near_xneg_yneg] - points[frust::near_xpos_yneg];
+			vectorTwo = points[frust::far_xpos_yneg] - points[frust::near_xpos_yneg];
+			planeNormals[frust::y_neg] = vectorOne.cross(vectorTwo);
+			planeNormalBases[frust::y_neg] = points[frust::near_xpos_yneg];
+
+			//x_neg
+			vectorOne = points[frust::near_xneg_ypos] - points[frust::near_xneg_yneg];
+			vectorTwo = points[frust::far_xneg_yneg] - points[frust::near_xneg_yneg];
+			planeNormals[frust::x_neg] = vectorOne.cross(vectorTwo);
+			planeNormalBases[frust::x_neg] = points[frust::near_xneg_yneg];
+
+			//y_pos
+			vectorOne = points[frust::near_xpos_ypos] - points[frust::near_xneg_ypos];
+			vectorTwo = points[frust::far_xneg_ypos] - points[frust::near_xneg_ypos];
+			planeNormals[frust::y_pos] = vectorOne.cross(vectorTwo);
+			planeNormalBases[frust::y_pos] = points[frust::near_xneg_ypos];
+
+			//z_near
+			vectorOne = points[frust::near_xneg_ypos] - points[frust::near_xpos_ypos];
+			vectorTwo = points[frust::near_xpos_yneg] - points[frust::near_xpos_ypos];
+			planeNormals[frust::z_near] = vectorOne.cross(vectorTwo);
+			planeNormalBases[frust::z_near] = points[frust::near_xpos_ypos];
+
+			//z_far
+			vectorOne = points[frust::far_xneg_yneg] - points[frust::far_xpos_yneg];
+			vectorTwo = points[frust::far_xpos_ypos] - points[frust::far_xpos_yneg];
+			planeNormals[frust::z_far] = vectorOne.cross(vectorTwo);
+			planeNormalBases[frust::z_far] = points[frust::far_xpos_yneg];
+
 		}
 
 	};
